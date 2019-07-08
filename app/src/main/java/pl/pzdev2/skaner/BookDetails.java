3b037cb27ced7dbeb9f6d57b7df111e4d1f268b9
pzdev2 <pzdev2@gmail.com>
2019-07-08 11:04:31 +0200

    Set 'main layout' and add 'detail layout'

---
+++ app/src/main/java/pl/pzdev2/skaner/BookDetails.java
@@ -0,0 +1,14 @@
+package pl.pzdev2.skaner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
