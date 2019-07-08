403e815f7d14c4e95ee0780e7dd1c107956b461f
pzdev2 <pzdev2@gmail.com>
2019-07-08 11:49:31 +0200

    Add comments to MainActivity.java

--- app/src/main/java/pl/pzdev2/skaner/MainActivity.java
+++ app/src/main/java/pl/pzdev2/skaner/MainActivity.java
@@ -2,7 +2,12 @@ package pl.pzdev2.skaner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+
+//                TextView item = (TextView) v;
+//                textView.setText(item.getText());
+                if(position == 0) {
+                    Intent intent = new Intent(MainActivity.this, BookDetails.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_books);
+        listView.setOnItemClickListener(itemClickListener);
     }
+
 }
