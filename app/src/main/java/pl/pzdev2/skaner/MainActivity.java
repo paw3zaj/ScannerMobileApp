fea718e052c7868f106b6da536882f8dd026a49b
pzdev2 <pzdev2@gmail.com>
2019-07-12 09:20:34 +0200

    SQLite complete

--- app/src/main/java/pl/pzdev2/skaner/MainActivity.java
+++ app/src/main/java/pl/pzdev2/skaner/MainActivity.java
@@ -3,35 +3,76 @@ package pl.pzdev2.skaner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//
+
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_books);
+
+        SQLiteOpenHelper databaseHepler = new DatabaseHelper(this);
+        try {
+            db = databaseHepler.getReadableDatabase();
+            cursor = db.query("BORROWED",
+                    new String[] {"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listView.setAdapter(listAdapter);
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
         //Create an OnItemClickListener
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
 
-//                TextView item = (TextView) v;
-//                textView.setText(item.getText());
-                if(position == 0) {
-                    Intent intent = new Intent(MainActivity.this, BookDetails.class);
-                    startActivity(intent);
-                }
+                //Pass the drink the user clicks on to DrinkActivity
+                Intent intent = new Intent(MainActivity.this,
+                        BookDetails.class);
+                intent.putExtra(BookDetails.EXTRA_BORROWID, (int) id);
+                startActivity(intent);
+
+//                if(position == 0) {
+//                    Intent intent = new Intent(MainActivity.this, BookDetails.class);
+//                    startActivity(intent);
+//                }
             }
         };
-        //Add the listener to the list view
-        ListView listView = (ListView) findViewById(R.id.list_books);
         listView.setOnItemClickListener(itemClickListener);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
 }
